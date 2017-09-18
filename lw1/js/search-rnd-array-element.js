167ec5310be34417be35e7c958002c57124f70cb
NightDK <31991690+NightDK@users.noreply.github.com>
2017-09-18 12:10:59 +0300

    Update search-rnd-array-element.js
    Добавил "Пробелы" и ";". Изменил объявление переменных.

--- lw1/js/search-rnd-array-element.js
+++ lw1/js/search-rnd-array-element.js
@@ -1,35 +1,36 @@
 const N = 10;
-var  array = []
-var searchNumber, IsFound, i;
-var fullmass = " ";
+var array = [],
+  searchNumber, 
+  IsFound = false,
+  i,
+  fullmass = " ";
 
-IsFound = false;
-  for (i = 0; i < N; i++) {
-    array[i] = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
+for (i = 0; i < N; i++) {
+  array[i] = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
+}
+
+do {
+  searchNumber = prompt("search namber: ");
+  if ((searchNumber == "") || (isNaN(searchNumber)) || searchNumber <= 0) {
+    alert("Ошибка ввода");
   }
-  
-  do {
-	searchNumber = prompt("search namber: ");
-	if ((searchNumber == "") || (isNaN(searchNumber)) || searchNumber<=0) {
-	  alert("Ошибка ввода");
-	}
-  } while((searchNumber == "") || (isNaN(searchNumber)) || searchNumber<=0);
-  
-  for (i = 0; i < N; i++) {
-    if (array[i] == searchNumber) {
-      IsFound = true;
-      break;
-    }
+} while ((searchNumber == "") || (isNaN(searchNumber)) || searchNumber <= 0);
+
+for (i = 0; i < N; i++) {
+  if (array[i] == searchNumber) {
+    IsFound = true;
+    break;
   }
-  
-  for (i = 0; i < N; i++) {
-	fullmass = fullmass + array[i] + " ";
-  }
-  
-  alert(fullmass);
-  
-  if (!IsFound) {
-	alert("Element " + searchNumber + " not found");
-  }else {
-    alert("Element " + searchNumber + " is found!");
-  }
+}
+
+for (i = 0; i < N; i++) {
+  fullmass = fullmass + array[i] + " ";
+}
+
+alert(fullmass);
+
+if (!IsFound) {
+  alert("Element " + searchNumber + " not found");
+} else {
+  alert("Element " + searchNumber + " is found!");
+}
