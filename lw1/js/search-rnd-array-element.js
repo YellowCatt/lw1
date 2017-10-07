8cc20de9bd4f680800a698a832607856f407fa09
NightDK <31991690+NightDK@users.noreply.github.com>
2017-10-07 14:13:00 +0300

    Исправлен выход из программы при вводе "0"

--- lw1/js/search-rnd-array-element.js
+++ lw1/js/search-rnd-array-element.js
@@ -1,23 +1,18 @@
 const N = 10;
-var array = [],
-  searchNumber, 
-  IsFound = false,
-  i,
-  fullmass = " ";
+var array = [], searchNumber, IsFound = false, i, fullmass = " ";
 
 for (i = 0; i < N; i++) {
-  array[i] = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
+  array[i] = Math.floor(Math.random() * (15 - 0 + 0)) + 0;
   fullmass = fullmass + array[i] + " ";
 }
 
-do {
   searchNumber = prompt("search namber: ");
-  if ((searchNumber == "") || (isNaN(searchNumber)) || searchNumber < 0) {
-    alert("Ошибка ввода");
+  if ((searchNumber == "") || (isNaN(searchNumber)) || searchNumber == null) {
+    alert("Error");
+    windows.stop;
   }
-} while ((searchNumber == "") || (isNaN(searchNumber)) || searchNumber < 0);
 
-if (searchNumber > 0) {
+if (searchNumber >= 0) {
   for (i = 0; i < N; i++) {
     if (array[i] == searchNumber) {
       IsFound = true;
