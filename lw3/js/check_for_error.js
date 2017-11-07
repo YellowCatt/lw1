f4a5f800890f0206503ffaadf487c3e5f750ca12
NightDK <mishail-99@mail.ru>
2017-11-07 16:27:47 +0300

    Создание функций
    для проверки, успешной регистрации, ошибки

--- lw3/js/check_for_error.js
+++ lw3/js/check_for_error.js
@@ -6,9 +6,23 @@ class RegistrationPage {
     this.formPasswordCheck = formElement.getElementsByClassName('input-pass-check__input')[0].value;
     this.formCheck = formElement.getElementsByClassName('check_input')[0].checked;
   }
+  validate() {
+  }
+  success() {
+    alert('Вы успешно зарегистрировались.');
+  }
+  error() {
+    alert('Error');
+  }
 }
 
 document.querySelector('.login-page').onsubmit = function () {
   var form = new RegistrationPage(document.getElementsByClassName('login-page')[0]);
-  
+  if (form.validate()) {
+    form.success();
+  }
+  else {
+    form.error();
+  }
+  return false;
 };
