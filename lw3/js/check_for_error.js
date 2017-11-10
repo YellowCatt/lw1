8973831b5f0c0d3cb23bfcca212dd1949be7abc8
NightDK <mishail-99@mail.ru>
2017-11-10 22:30:13 +0300

    создание ф-ии Validate

--- lw3/js/check_for_error.js
+++ lw3/js/check_for_error.js
@@ -1,54 +1,57 @@
-function validLogin(email) {
-  var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-  return pattern.test(email);
-}
+function registration(event) {
+  
+  var Email = document.getElementsByClassName('input-log')[0].value;
+  var Password = document.getElementsByClassName('input-pass')[0].value;
+  var PasswordCheck = document.getElementsByClassName('input-pass-check')[0].value;
+  var Check = document.getElementsByClassName('check_input')[0].checked;
+  var errorMsg = "null";
 
-class RegistrationPage {
-  constructor(formElement) {
-    this.form = formElement;
-    this.formLogin = formElement.getElementsByClassName('input-log__input')[0].value;
-    this.formPassword = formElement.getElementsByClassName('input-pass__input')[0].value;
-    this.formPasswordCheck = formElement.getElementsByClassName('input-pass-check__input')[0].value;
-    this.formCheck = formElement.getElementsByClassName('check_input')[0].checked;
-  }
-  validate() {
-    if (!validLogin(this.formLogin)) {
-      this.errorMsg = 'Введите корректный email!';
-      return false;
-    }
-
-    if (this.formPassword.length < 6) {
-      this.errorMsg = 'Длина пароля должна составлять более 6 символов';
-      return false;
-    }
-
-    if (this.formPassword !== this.formPasswordCheck) {
-      this.errorMsg = 'Пароли не совпадают!';
-      return false;
-    }
-
-    if (!this.formCheck) {
-      this.errorMsg = 'Вы не согласны с правилами сайта!';
-      return false;
-    }
-
-    return true;
-  }
-  success() {
-    alert('Вы успешно зарегистрировались.');
-  }
-  error() {
-    alert(this.errorMsg);
-  }
-}
-
-document.querySelector('.login-page').onsubmit = function () {
-  var form = new RegistrationPage(document.getElementsByClassName('login-page')[0]);
-  if (form.validate()) {
-    form.success();
+  if (validate(Email, Password, PasswordCheck, Check)) {
+    success();
   }
   else {
-    form.error();
+    error();
+    event.preventDefault();
   }
-  return false;
-};
+}
+
+function validate(Email, Password, PasswordCheck, Check) {
+  if (!validEmail(Email)) {
+    errorMsg = 'Введите корректный email!';
+    return false;
+  }
+
+  if (Password.length < 6) {
+    errorMsg = 'Длина пароля должна составлять более 6 символов';
+    return false;
+  }
+
+  if (Password !== PasswordCheck) {
+    errorMsg = 'Пароли не совпадают!';
+    return false;
+  }
+
+  if (!Check) {
+    errorMsg = 'Вы не согласны с правилами сайта!';
+    return false;
+  }
+
+  return true;
+}
+
+function validEmail(Email) {
+  var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+  return pattern.test(Email);
+}
+
+function success() {
+  alert('Вы успешно зарегистрировались.');
+}
+
+function error() {
+  alert(errorMsg);
+}
+
+window.onload = function() {
+  document.getElementById('login-page').addEventListener('submit', registration);
+}
