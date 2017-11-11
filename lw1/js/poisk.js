d98c4dfcad2ef41bf666a971c42129c2fb1b3946
YellowCatt <>
2017-11-11 14:35:18 +0300

    add programm

---
+++ lw1/js/poisk.js
@@ -0,0 +1,39 @@
+var array = [],
+  poisk = " ",
+  isFound = false,
+  i, fullmass = " ",
+  N = 10,
+  inputError;
+ 
+
+for (i = 0; i < N; i++) {
+  array[i] = Math.floor(Math.random() * (10 - 0 + 1 ) + 0 );
+  fullmass = fullmass + array[i] + " ";
+}
+
+do {
+  poisk = prompt("Введите число: ");
+  inputError = false;
+  if (poisk !== null) {
+    if ((poisk == "") || (isNaN(poisk)))
+     {
+      alert("Ошибка");
+      inputError = true;
+    }
+  }
+} while (inputError == true);
+
+if (poisk !== null) {
+  for (i = 0; i < N; i++) {
+    if (array[i] == poisk) {
+      isFound = true;
+    }
+  }
+  alert(fullmass);
+
+  if (!isFound) {
+    alert("Элемент " + poisk + " не найден");
+  } else {
+    alert("Элемент " + poisk + " найден");
+  }
+}
