a52d67c1eef32cfe8722d0ca14c664209c976830
YellowCatt <32128092+YellowCatt@users.noreply.github.com>
2017-12-02 13:41:04 +0300

    Update search-rnd-array-element.js

--- lw1/js/search-rnd-array-element.js
+++ lw1/js/search-rnd-array-element.js
@@ -7,15 +7,14 @@ var array = [],
  
 
 for (i = 0; i < N; i++) {
-  array[i] = Math.floor(Math.random() * (10 - 0 + 1 ) + 0 );
+  array[i] = Math.floor(Math.random() * (N - 0 + 1 ) + 0 );
   fullmass = fullmass + array[i] + " ";
 }
 
 do {
   searchNumber = prompt("Введите число:");
   inputError = false;
-  if ((searchNumber !== null)&&(searchNumber == "") || (isNaN(searchNumber)))
-    { 
+  if ((searchNumber !== null) && (searchNumber == "") || (isNaN(searchNumber))){ 
       alert("Ошибка");
       inputError = true;
     }
